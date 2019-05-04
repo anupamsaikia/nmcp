0d861d53b823f43bc6391f25a55e085e56faab9d
Anupam Saikia <ianupamsaikia@gmail.com>
2019-05-04 23:43:21 +0530

    removed unnecessary variables : yet to test

--- euler.c
+++ euler.c
@@ -9,7 +9,7 @@ float f(float x, float y)
 int main()
 {
   int i = 0;
-  float x0, y0, x1, y1, xn, h;
+  float x0, y0, xn, h;
   printf("Enter the initial value of x and y :\n");
   scanf("%f%f", &x0, &y0);
   printf("Enter the step length :");
@@ -19,12 +19,9 @@ int main()
 
   while (x0 <= xn)
   {
-    y1 = y0 + h * f(x0, y0);
-    x1 = x0 + h;
-    x0 = x1;
-    y0 = y1;
-
     printf("\nWhen x(%d) = %f\tThen y(%f) = %f", i++, x0, x0, y0);
+    y0 = y0 + h * f(x0, y0);
+    x0 = x0 + h;
   }
 
   printf("\n\n");
