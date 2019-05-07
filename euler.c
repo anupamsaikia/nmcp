61eac2bd9fc12984e231c257d9ca2f09a3aa74a3
Anupam Saikia <ianupamsaikia@gmail.com>
2019-05-07 21:07:10 +0530

    corrected and tested : working

--- euler.c
+++ euler.c
@@ -9,19 +9,19 @@ float f(float x, float y)
 int main()
 {
   int i = 0;
-  float x0, y0, xn, h;
+  float x, y, xn, h;
   printf("Enter the initial value of x and y :\n");
-  scanf("%f%f", &x0, &y0);
+  scanf("%f%f", &x, &y);
   printf("Enter the step length :");
   scanf("%f", &h);
   printf("Enter the value of x for which y is required :");
   scanf("%f", &xn);
 
-  while (x0 <= xn)
+  while (x <= xn)
   {
-    printf("\nWhen x(%d) = %f\tThen y(%f) = %f", i++, x0, x0, y0);
-    y0 = y0 + h * f(x0, y0);
-    x0 = x0 + h;
+    y += h * f(x, y);
+    x += h;
+    printf("\nWhen x%d = %f\tThen y(%f) = %f", ++i, x, x, y);
   }
 
   printf("\n\n");
