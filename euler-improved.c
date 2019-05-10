53af74a096f097377c7ef4a36254182f26ad153a
Anupam Saikia <ianupamsaikia@gmail.com>
2019-05-10 23:41:32 +0530

    removed unused header file, corrected the logic

--- euler-improved.c
+++ euler-improved.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <math.h>
 
 float f(float x, float y)
 {
@@ -8,26 +7,25 @@ float f(float x, float y)
 
 int main()
 {
-  int i = 0, n;
+  int n;
   float x0, y0, x1, y11, y12, xn, h;
   printf("Enter the initial value of x0 and y0 :\n");
   scanf("%f%f", &x0, &y0);
-  printf("Enter the value of xn for which ans is required : ");
+  printf("Enter the value of x for which y is required : ");
   scanf("%f", &xn);
   printf("Enter the number of intervals n : ");
   scanf("%d", &n);
   
   h = (xn - x0)/n;
 
-  while (x0 <= xn)
+  while (x0 < xn)
   {
-    printf("\nWhen x(%d) = %f\tThen y(%f) = %f", i++, x0, x0, y0);
-    y11 = y0 + h * f(x0, y0);
     x1 = x0 + h;
+    y11 = y0 + h * f(x0, y0);
     y12 = y0 + (h/2)*(f(x0,y0) + f(x1,y11));
+    printf("\nWhen x = %g\tThen y(%g) = %f", x1, x1, y12);
     x0 = x1;
-    y11 = y12;
-    y0 = y11;
+    y0 = y12;
   }
 
   printf("\n\n");
